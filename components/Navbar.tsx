ab3b94770e7c64877fda55b71ba3a27c83d680b1
iqra fareed <iqrafareed56@gmail.com>
2023-06-11 15:52:00 +0500

    nav fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <CustomButton
           title="Sign In"
           btnType="button"
-          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px] z-10"
         />
       </nav>
     </header>
